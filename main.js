ab46819923c9d291e24f1dd48fdc38a715b56f11
Emelike Adaeze <emelikepatience961@gmail.com>
2022-08-24 01:36:05 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+let home = document.getElementById('home')
+let result = 0
+function incrementby1() {
+    result = result + 1;
+    home.innerHTML = result;
+}
+
+function incrementby2() {
+    result = result + 2;
+    home.innerHTML = result;
+}
+
+function incrementby3() {
+    result = result + 3;
+    home.innerHTML = result;
+}
+
+
+
+let guest = document.getElementById('guest')
+let results = 0
+function increment1() {
+    results = results + 1;
+    guest.innerHTML = results;
+}
+
+function increment2() {
+    results = results + 2;
+    guest.innerHTML = results;
+}
+
+function increment3() {
+    results = results + 3;
+    guest.innerHTML = results;
+}
